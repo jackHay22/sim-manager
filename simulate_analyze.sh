8b7912ed0b8c31a74e4dc533e6f2de7631bae794
jackHay22 <jjphay@gmail.com>
2021-10-10 15:28:46 -0400

    update script

--- simulate_analyze.sh
+++ simulate_analyze.sh
@@ -10,6 +10,7 @@ echo "   -n $1/$3.net.xml "
 echo "   -r $1/$3.rou.xml "
 echo "   -a $1/$3.towers.xml "
 echo "   --bt-output $2/${3}.bt_output.xml "
+echo "   --netstate-dump $2/${3}.ns_output.xml"
 echo "   --device.btreceiver.range 1000.0 "
 echo "   --device.btreceiver.all-recognitions"
 #Run the simulation
@@ -18,6 +19,7 @@ $SUMO_HOME/bin/sumo \
       -r $1/$3.rou.xml \
       -a $1/$3.towers.xml \
       --bt-output $2/${3}.bt_output.xml \
+      --netstate-dump $2/${3}.ns_output.xml \
       --device.btreceiver.range 1000.0 \
       --device.btreceiver.all-recognitions
 
@@ -27,9 +29,11 @@ echo "mkdir -p $OUT_DIR"
 mkdir -p $OUT_DIR
 echo "./analysis/analysis_transformer.o "
 echo "   -b $2/${3}.bt_output.xml "
+echo "   -r $2/${3}.ns_output.xml"
 echo "   -n $1/${3}.net.xml "
 echo "   -o $OUT_DIR"
 ./analysis/analysis_transformer.o \
     -b $2/${3}.bt_output.xml \
+    -r $2/${3}.ns_output.xml \
     -n $1/${3}.net.xml \
     -o $OUT_DIR
